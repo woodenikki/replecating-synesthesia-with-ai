e500902869d0cfc7b71074d0ce4ebe32bc369660
Nikki Wood <woodenikki@gmail.com>
2021-04-07 23:56:16 -0600

    Works, but is clunky. But it WORKS

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,7 @@
 import os
 import json
 import math
+import click
 import librosa
 import numpy as np
 import tensorflow as tf
@@ -13,31 +14,61 @@ COLOR_CATEGORIES = ["Black", "Blue", "Brown", "Gray", "Green", "Orange", "Pink",
 TEXTURE_CATEGORIES = ["None", "Smoky", "Smooth", "Soft", "Sparkly", "Spiky", "Woody"]
 LUMINOSITY_CATEGORIES = ["Yes", "No"]
 
-SAMPLE_RATE             = 22050                     # music processing value
-DURATION                = 30                        # measured in seconds 
-SAMPLES_PER_TRACK       = SAMPLE_RATE * DURATION
-
 DATASET_PATH            = "E:\Test"
-TEST_SAMPLE             = "E:\Test\classical.00013.wav"
 COLOR_MODEL             = "src\models\color300.h5"
-TEXTURE_MODEL           = "src\models\texture300.h5"
+TEXTURE_MODEL           = "src\\models\\texture300.h5"
 LUMINOSITY_MODEL        = "src\models\luminisity75.h5"
 
+@click.command()
+@click.option("-s", "--sample", required=False, help="Name of test sample (including .wav) from test_samples folder")
+@click.option(
+    "-p",
+    "--path",
+    default="src\\test_samples\\TESTING",
+    type=click.Path(file_okay=False, writable=False, exists=True),
+    help="Path to find test samples",
+)
+def main(sample, path):
+    
+    inputs, names = load_data(path)
+    # print(inputs[0].size)
+
+    col_model = tf.keras.models.load_model(COLOR_MODEL)
+    col_model.summary()
+
+    tex_model = tf.keras.models.load_model(TEXTURE_MODEL)
+    tex_model.summary()
+
+    lum_model = tf.keras.models.load_model(LUMINOSITY_MODEL)
+    lum_model.summary()
+
+    song = names[0]
+    col = COLOR_CATEGORIES[np.argmax(col_model.predict(inputs), axis=-1)[0]]
+    tex = TEXTURE_CATEGORIES[np.argmax(tex_model.predict(inputs), axis=-1)[0]]
+    lum_bool = LUMINOSITY_CATEGORIES[np.argmax(lum_model.predict(inputs), axis=-1)[0]]
+    if (lum_bool == "Yes"):
+        lum = ""
+    else:
+        lum = " no"
+
+    if (tex == "None"):
+        tex = "Untextured"
+
+    print("{} is predicted to be {} {} with{} luminosity".format(song, tex, col, lum))
+
+
+
 def load_data(dataset_path, num_segments=10, n_mfcc=13, n_fft=2048, hop_length=512):
-    """Extracts MFCCs from music dataset and saves them into a json file along with genre labels.
-        :param dataset_path (str): Path to dataset
-        :param json_path (str): Path to json file used to save MFCCs
-        :param num_mfcc (int): Number of coefficients to extract
-        :param n_fft (int): Interval we consider to apply FFT. Measured in # of samples
-        :param hop_length (int): Sliding window for FFT. Measured in # of samples
-        :param: num_segments (int): Number of segments we want to divide sample tracks into
-        :return:
-        """
+
+    SAMPLE_RATE             = 22050                     # music processing value
+    DURATION                = 30                        # measured in seconds 
+    SAMPLES_PER_TRACK       = SAMPLE_RATE * DURATION
     # build a dictionary to store data
     data = {
         "mapping": [],                                  # "classical" (-> 0), "blues (-> 1), etc...
         "mfcc": [],                                     # training inputs
-        "labels": []                                    # training targets [0, 0, 1]
+        "labels": [],                                   # training targets [0, 0, 1]
+        "names": []
     }
 
     num_samples_per_segment                 = int(SAMPLES_PER_TRACK / num_segments)
@@ -58,7 +89,6 @@ def load_data(dataset_path, num_segments=10, n_mfcc=13, n_fft=2048, hop_length=5
                 start_sample = num_samples_per_segment * s
                 finish_sample = start_sample + num_samples_per_segment 
                 
-
                 # extract mfcc
                 mfcc = librosa.feature.mfcc(signal[start_sample:finish_sample], sr=sr, n_fft=n_fft, n_mfcc=n_mfcc, hop_length=hop_length)
                 mfcc = mfcc.T
@@ -67,19 +97,17 @@ def load_data(dataset_path, num_segments=10, n_mfcc=13, n_fft=2048, hop_length=5
                 if len(mfcc) == expected_num_mfcc_vectors_per_segment:
                     data["mfcc"].append(mfcc.tolist())
                     data["labels"].append(i-1)
+                    data["names"].append(f) #.split("."[0])
                     print("{}, segment:{}".format(file_path, s+1))
 
     # convert lists to numpy arrays
     inputs = np.array(data["mfcc"])
+    names = np.array(data["names"])
 
-    return  inputs
+    return  inputs, names
+
+
 
 if __name__ == "__main__":
-
-    inputs = load_data(DATASET_PATH)
-    print(inputs[0].size)
-    model = tf.keras.models.load_model(LUMINOSITY_MODEL)
-    model.summary()
-
-    print(LUMINOSITY_CATEGORIES[np.argmax(model.predict(inputs), axis=-1)[0]])
-    # print(LUMINOSITY_CATEGORIES[np.argmax(model.predict(inputs), axis=-1)[0]])
+    main()
+   
